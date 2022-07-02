baf8adfe29d62a9ebb36874754fd494ca3c92760
Ade2002 <sikirusulaimon2002@gmail.com>
2022-07-02 12:56:28 +0100

    javascript

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ inputs.forEach((input) =>
   input.addEventListener("change", (e) => {
     label = e.target.closest(".userBox").querySelector("label");
     if (input.value != "") {
-      label.style.top = "-30px";
+      label.style.top = "-20px";
+      label.style.color = "#" + "03e9f4";
+      label.style.fontSize= "12px";
       console.log(input.value);
     }else{
         label.style.top = "0px";
